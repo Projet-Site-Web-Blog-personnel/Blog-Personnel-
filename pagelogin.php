bf60bd20df8ce91647d623f8ee5807dbf3c00461
ibrahimasorydiallo1 <isddiallo2003@gmail.com>
2023-04-06 17:26:30 +0200

    Code à vérifier

--- pagelogin.php
+++ pagelogin.php
@@ -9,9 +9,9 @@
 </head>
 
 <body>
-<header>
+    <header>
         <div class="logo left">
-            <img src="img/logo.png" alt="logo's picture" class="left2" >
+            <img src="img/logo.png" alt="logo's picture" class="left2">
         </div>
         <div class="title">
             <h1> WINOX</h1>
@@ -78,28 +78,12 @@
 
         $email = $_POST['email'];
         $mot_de_passe = $_POST['mot_de_passe'];
-        $db = new PDO('mysql:host=localhost;dbname=projetannuel;charset=utf8', 'root', '');
+        $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
         $stmt = $db->prepare("SELECT * FROM utilisateur WHERE email = :email AND mot_de_passe = :mot_de_passe");
         $stmt->bindParam(':email', $email);
         $stmt->bindParam(':mot_de_passe', $mot_de_passe);
         $stmt->execute();
 
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        if ($row) {
-            $ID_Role = $row['ID_Role'];
-            if ($ID_Role == 1) {
-                header("Location: pageadmin.php");
-            } elseif ($ID_Role == 2) {
-                header("Location: pagecompta.php");
-            } elseif ($ID_Role == 3) {
-                header("Location: pageuser.php");
-            }
-        } else {
-
-            // Pour afficher un message d'alerte si la connexion est impossible
-            echo '<div class="alert alert-danger" role="alert"> Connexion impossible, êtes vous inscrit ? <a href="mailto:?to=admin@entreprise.com &subject=Identifiant%20non%20enregistré &body=Bonjour,%20mes%20identifiants%20n ont%20pas%20été%20saisi."> Si vous n\'avez pas d\'identifiant </a>. Cliquez pour vous inscrire. </div>';
-        }
 
         echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
               <strong>C est nickel</strong> l ajout est OK.
