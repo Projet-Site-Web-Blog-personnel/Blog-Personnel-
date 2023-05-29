ebca2e2c8116115024a8bc8356717b7a8cb68494
Atigou-Bah <atigoubah2003@gmail.com>
2023-05-29 17:39:09 +0200

    Ajout de la page Acceuil

---
+++ acceuil.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="pagelogin.css">
+    <title>Document</title>
+</head>
+<body>
+<header>
+        <div class="logo left">
+            <img src="img/logo.png" alt="logo's picture" class="left2">
+        </div>
+        <div class="search">
+            <input type="search" name="" id=""  placeholder="recherche">
+        </div>
+        <div class="nav_links">
+         <div class="nav_links_position">
+            <p> A propos</p>
+         </div>
+         <div class="nav_links_position">
+            <p>connexion</p>
+         </div>
+         <div class="nav_links_position">
+            <button> S'inscrire </button>
+         </div>
+         </div>
+    </header>
+</body>
+</html>
