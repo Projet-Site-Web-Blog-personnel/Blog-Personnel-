802266479337cc69683299bbc2831eb4241e0978
ibrahimasorydiallo1 <isddiallo2003@gmail.com>
2023-06-06 15:35:55 +0200

    Ajout fonctionnalité email

--- enregistrement.php
+++ enregistrement.php
@@ -72,31 +72,63 @@
                 <input type="submit" value="Create">
         </div>
         <?php
-        if (isset($_POST['email']) && isset($_POST['mot_de_passe']) && isset($_POST['ville']) && isset($_POST['Nom']) && isset($_POST['Prenom'])) {
+            use PHPMailer\PHPMailer\PHPMailer;
+            use PHPMailer\PHPMailer\Exception;
 
-            $email = $_POST['email'];
-            $mot_de_passe = $_POST['mot_de_passe'];
-            $hashed_password = password_hash($mot_de_passe, PASSWORD_DEFAULT);
-            $ville = $_POST['ville'];
-            $Nom = $_POST['Nom'];
-            $Prenom = $_POST['Prenom'];
+            // Chargement des classes PHPMailer
+            require_once 'PHPMailer/src/PHPMailer.php';
+            require_once 'PHPMailer/src/SMTP.php';
+            require_once 'PHPMailer/src/Exception.php';
 
-            $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
-            $stmt = $db->prepare("INSERT INTO utilisateur (email, mot_de_passe, Nom, Prenom,ville) VALUES (:email, :mot_de_passe, :Nom, :Prenom, :ville)");
-            $stmt->bindParam(':mot_de_passe', $mot_de_passe);
-            $stmt->bindParam(':mot_de_passe', $hashed_password);
-            $stmt->bindParam(':email', $email);
-            $stmt->bindParam(':Prenom', $Prenom);
-            $stmt->bindParam(':Nom', $Nom);
-            $stmt->bindParam(':ville', $ville);
+            if (isset($_POST['email']) && isset($_POST['mot_de_passe']) && isset($_POST['ville']) && isset($_POST['Nom']) && isset($_POST['Prenom'])) 
+            {
+                $email = $_POST['email'];
+                $mot_de_passe = $_POST['mot_de_passe'];
+                $hashed_password = password_hash($mot_de_passe, PASSWORD_DEFAULT);
+                $ville = $_POST['ville'];
+                $Nom = $_POST['Nom'];
+                $Prenom = $_POST['Prenom'];
 
-            $stmt->execute();
+                $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+                $stmt = $db->prepare("INSERT INTO utilisateur (email, mot_de_passe, Nom, Prenom,ville) VALUES (:email, :mot_de_passe, :Nom, :Prenom, :ville)");
+                $stmt->bindParam(':mot_de_passe', $mot_de_passe);
+                $stmt->bindParam(':mot_de_passe', $hashed_password);
+                $stmt->bindParam(':email', $email);
+                $stmt->bindParam(':Prenom', $Prenom);
+                $stmt->bindParam(':Nom', $Nom);
+                $stmt->bindParam(':ville', $ville);
 
-            echo "<script> alert('Utilisateur bien ajouté') </script>";
-        }
+                $stmt->execute();
+
+                echo "<script> alert('Utilisateur bien ajouté') </script>";
+
+                // Configuration de PHPMailer
+                $mail = new PHPMailer(true);
+                $mail->isSMTP();
+                $mail->Host = 'smtp.gmail.com';
+                $mail->SMTPAuth = true;
+                $mail->Username = 'equipedevdufutur2023@gmail.com';
+                $mail->Password = 'ntdvhzvqkelbdmrm';
+                $mail->SMTPSecure = 'ssl';
+                $mail->Port = 465;
+
+                // Définition des en-têtes du message électronique
+                $mail->setFrom('equipeDevDuFutur2023@gmail.com', 'Equipe de choc');
+                $mail->addAddress($email, $Prenom);
+                $mail->isHTML (true);
+                $mail->Subject = 'Bienvenue sur WINOX';
+                $mail->Body    = "Bonjour $Prenom, \n\n F&eacute;licitation , tu es d&eacute;sormais chez nous !! Voici ton mot de passe pour te connecter: $mot_de_passe .";
+
+                // Envoi de l'e-mail
+                if(!$mail->send()) {
+                    echo 'Erreur : ' . $mail->ErrorInfo;
+                } else {
+                    echo 'Message envoyé avec succès';
+                }
+            }
         ?>
 
-        <!-- <?php
+         <?php
                 function validatePassword($password)
                 {
                     $uppercase = preg_match('@[A-Z]@', $password);
@@ -110,13 +142,13 @@
                         return true;
                     }
                 }
-                ?> -->
+            ?> 
 
-        </form> <!-- closing tag for form element -->
+        </form>
 
 
 
-    </div> <!-- closing tag for box div -->
+    </div> 
 </body>
 
 </html>
